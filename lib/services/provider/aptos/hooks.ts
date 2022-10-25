a45bce12e464a9e0dbfc42ac8b58b1cec3d471d6
Deckard <103876178+deckard1982@users.noreply.github.com>
2022-10-25 12:20:45 +0800

    Draft aptos transaction request 3

---
+++ lib/services/provider/aptos/hooks.ts
@@ -0,0 +1,75 @@
+import { TxnBuilderTypes, Types } from 'aptos'
+import { useAsyncRetry, useInterval } from 'react-use'
+
+import { IChainAccount, INetwork } from '~lib/schema'
+import { getAptosClient } from '~lib/services/provider/aptos/client'
+import { AptosProvider } from '~lib/services/provider/aptos/provider'
+
+export function useAptosTransaction(
+  network?: INetwork,
+  account?: IChainAccount,
+  rawTx?: TxnBuilderTypes.RawTransaction,
+  dontEstimateGasPrice?: boolean,
+  dontEstimateGasLimit?: boolean,
+  expirationSecs?: number
+) {
+  const { value, loading, retry } = useAsyncRetry(async () => {
+    if (!network || !account || !rawTx) {
+      return
+    }
+    const client = await getAptosClient(network)
+    if (!client) {
+      return
+    }
+
+    const provider = new AptosProvider(client)
+
+    let {
+      sender,
+      sequence_number,
+      payload,
+      max_gas_amount,
+      gas_unit_price,
+      expiration_timestamp_secs,
+      chain_id
+    } = rawTx
+
+    if (typeof expirationSecs === 'number') {
+      const expireTimestamp = Math.floor(Date.now() / 1000) + expirationSecs
+      expiration_timestamp_secs = BigInt(expireTimestamp)
+    }
+    const rawTransaction = new TxnBuilderTypes.RawTransaction(
+      sender,
+      sequence_number,
+      payload,
+      max_gas_amount,
+      gas_unit_price,
+      expiration_timestamp_secs,
+      chain_id
+    )
+
+    const userTxs = await provider.simulateTransaction(
+      account,
+      rawTransaction,
+      {
+        estimateGasUnitPrice: !dontEstimateGasPrice,
+        estimateMaxGasAmount: !dontEstimateGasLimit
+      }
+    )
+    const userTransaction = userTxs[0]
+
+    console.log(userTransaction)
+
+    return {
+      rawTransaction,
+      userTransaction
+    } as {
+      rawTransaction: TxnBuilderTypes.RawTransaction
+      userTransaction: Types.UserTransaction
+    }
+  }, [network, account, rawTx, expirationSecs])
+
+  useInterval(retry, !loading ? 5000 : null)
+
+  return value
+}
