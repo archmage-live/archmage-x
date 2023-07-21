954897b4355e67756b4e0321eaa956eac8e56444
Deckard <103876178+deckard1982@users.noreply.github.com>
2023-07-21 12:16:27 +0800

    Draft erc4337 account abstraction 7

---
+++ lib/safe/index.ts
@@ -0,0 +1,17 @@
+import { EthersAdapter } from '@safe-global/protocol-kit'
+import SafeApiKit from '@safe-global/api-kit'
+import { SafeFactory } from '@safe-global/protocol-kit'
+
+
+const SAFE_TX_SERVICE_URLS = new Map([
+  [1, 'https://safe-transaction-mainnet.safe.global'],
+  [5, 'https://safe-transaction-goerli.safe.global/'],
+  [137, 'https://safe-transaction-polygon.safe.global/'],
+  [42161, 'https://safe-transaction-arbitrum.safe.global/'],
+  [10, 'https://safe-transaction-optimism.safe.global/'],
+  [43114, 'https://safe-transaction-avalanche.safe.global/'],
+  [56, 'https://safe-transaction-bsc.safe.global/'],
+  [100, 'https://safe-transaction-gnosis-chain.safe.global/'],
+  [1313161554, 'https://safe-transaction-aurora.safe.global/'],
+  [84531, 'https://safe-transaction-base-testnet.safe.global/'],
+])
