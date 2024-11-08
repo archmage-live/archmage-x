3b2fc605cfbc7d5ed878471cfe2e3b89aa6b0bca
Deckard <103876178+deckard1982@users.noreply.github.com>
2024-11-08 20:00:31 +0800

    chore: remove node-linker=hoisted https://gist.github.com/Zn4rK/ed60c380e7b672e3089074f51792a2b8

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -2,24 +2,43 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { FileStore } = require('metro-cache');
 const path = require('path');
+const { makeMetroConfig } = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
 
 const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
+const monorepoRoot = path.resolve(projectRoot, '../..');
 
-const config = getDefaultConfig(projectRoot);
+const symlinksResolver = MetroSymlinksResolver();
 
-// Since we are using pnpm, we have to setup the monorepo manually for Metro
-// #1 - Watch all files in the monorepo
-config.watchFolders = [workspaceRoot];
-// #2 - Try resolving with project modules first, then workspace modules
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-];
+const expoConfig = getDefaultConfig(projectRoot);
 
-// Use turborepo to restore the cache when possible
-config.cacheStores = [
-  new FileStore({ root: path.join(projectRoot, 'node_modules', '.cache', 'metro') }),
-];
+/** @type {import('expo/metro-config').MetroConfig} */
+module.exports = makeMetroConfig({
+  ...expoConfig,
+  resolver: {
+    ...expoConfig.resolver,
+    resolveRequest: (context, moduleName, platform) => {
+      try {
+        // Symlinks resolver throws when it can't find what we're looking for.
+        const res = symlinksResolver(context, moduleName, platform);
 
-module.exports = config;
+        if (res) {
+          return res;
+        }
+      } catch {
+        // If we have an error, we pass it on to the next resolver in the chain,
+        // which should be one of expos.
+        // https://github.com/expo/expo/blob/9c025ce7c10b23546ca889f3905f4a46d65608a4/packages/%40expo/cli/src/start/server/metro/withMetroResolvers.ts#L47
+        return context.resolveRequest(context, moduleName, platform);
+      }
+    },
+  },
+  // Watch all files within the monorepo.
+  watchFolders: [monorepoRoot],
+  // Use turborepo to restore the cache when possible.
+  cacheStores: [
+    new FileStore({
+      root: path.join(projectRoot, 'node_modules', '.cache', 'metro'),
+    }),
+  ],
+});
